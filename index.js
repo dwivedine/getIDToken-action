2e6572f3fd06abc0d19cc42db6abc74fd347198c
Neha Dwivedi <76174073+dwivedine@users.noreply.github.com>
2021-07-01 12:00:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,24 +6,31 @@ const username = core.getInput('username', {required: false});
 /*const password = core.getInput('password', {required: true});
 const scope = core.getInput('scope', {required: true}); */
 
+const url = process.env['ACTIONS_ID_TOKEN_REQUEST_URL'];
+var bearer = 'Bearer ' + process.env['ACTIONS_RUNTIME_TOKEN'];
+
 //let data = {'grant_type':"password", 'client_id':client_id, 'username':username, 'password':password, 'scope':scope, 'client_secret':client_secret};
 //console.log(data);
 const fetch = require("node-fetch");
 const core = require('@actions/core');
 
 async function postData(){
-    const response = await fetch("https://ghactionsoidc.azurewebsites.net/connect/token/", {
+    const response = await fetch(url, {
         method: "POST", 
-        body: new URLSearchParams({'grant_type':'password', 'client_id':'ghactions', 'username':'demorepo', 'password':'HelloWorld123#', 'scope':'openid', 'client_secret':'HelloWorld123#' }),
-      });
+        withCredentials: true,
+        credentials: 'include',
+        headers: {
+            'Authorization': bearer
+        }
+    });
       return response.json();
       
 }
 
-/*postData()
+postData()
   .then(data => {
     core.setOutput('response_json', data);
-  });*/
+  });
 
-core.setOuput('response_json', username);
+//core.setOuput('response_json', username);
 
